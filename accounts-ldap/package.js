72c3dd0636167b9252e5467015c9882a9e1e3158
emgee3 <emgee@gravitronic.com>
2014-05-04 07:38:08 -0700

    update package and read me

--- accounts-ldap/package.js
+++ accounts-ldap/package.js
@@ -5,10 +5,11 @@ Package.describe({
 Npm.depends({'ldapjs' : '0.6.3'});
 
 Package.on_use(function (api) {
-  api.use(['accounts-urls', 'accounts-base', 'underscore', 'templating', 'bootstrap'], 'client');
-
-  api.add_files([
-  api.add_files(["ldap.js","ldap-handler.js"], "server");
+  api.use(['templating'], 'client');
+  api.use(['accounts-base', 'accounts-password'], 'server');
+  
+  api.add_files(["ldap-client.js","login-handler.js"], "server");
 
   api.export('LDAP', 'server');
 });
+
