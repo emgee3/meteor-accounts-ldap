f7547b57e483d07274ae9651e7bca6e3db17ac23
emgee3 <emgee@gravitronic.com>
2014-05-04 07:36:49 -0700

    remove cruft

--- accounts-ldap/package.js
+++ accounts-ldap/package.js
@@ -8,19 +8,6 @@ Package.on_use(function (api) {
   api.use(['accounts-urls', 'accounts-base', 'underscore', 'templating', 'bootstrap'], 'client');
 
   api.add_files([
-    'accounts_ui.js',
-
-    'login_buttons_images.css',
-    'login_buttons.html',
-    'login_buttons_dropdown.html',
-    'login_buttons_dialogs.html',
-
-    'login_buttons_session.js',
-
-    'login_buttons.js',
-    'login_buttons_dropdown.js',
-    'login_buttons_dialogs.js'], 'client');
-
   api.add_files(["ldap.js","ldap-handler.js"], "server");
 
   api.export('LDAP', 'server');
